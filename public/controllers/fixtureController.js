34986182f0b65fe7cb91f20f1728d9fec0856aba
WolvesDave <wolvesdave@gmail.com>
2018-05-04 11:21:59 -0400

    Added parms to loadFixtures and updated function

--- public/controllers/fixtureController.js
+++ public/controllers/fixtureController.js
@@ -27,84 +27,94 @@ predictionApp.controller('fixtureController', ['$scope', '$filter', '$http', fun
       $scope.fixture.fixtures.push({"homeTeam" : "", "awayTeam" : ""})
     };
 
-    $scope.populateFixtures = function() {
-      console.log("In populateFixtures", $scope.fixture);
+    $scope.populateFixtures = function(startDate, endDate) {
+      console.log("In populateFixtures", $scope.fixture, “start “, startDate, “end “, endDate);
       http.post = require('http-post');
 
       payload = {
         'ApiKey':'QQQMPBBYBPJYCVJCBHFRQMFVOCOSLBPPCXVGWLRKRRKAEACUXC',
         'seasonDateString':'1718',
         'league':'Scottish Premier League',
-  		'startDateString' : '2018-01-01',
-  		'endDateString' : '2018-07-31'
+  		'startDateString' : startDate,
+  		'endDateString' : endDate
       };
 
-      MongoClient.connect('mongodb://localhost:27017/predictionleague', function(err, db) {
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
 
-      /*  http.post('http://www.xmlsoccer.com/FootballDataDemo.asmx/GetFixturesByLeagueAndSeason', */
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  const db = client.db("prediction-league");
 
-        http.post('http://www.xmlsoccer.com/FootballDataDemo.asmx/GetFixturesByDateIntervalAndLeague',
-        payload, function(res){
+  post('http://www.xmlsoccer.com/FootballDataDemo.asmx/GetFixturesByDateIntervalAndLeague',
+  payload, function(res){
 
-            console.log(`STATUS: ${res.statusCode}`);
-            res.setEncoding('utf8');
-            console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+      console.log(`STATUS: ${res.statusCode}`);
+      res.setEncoding('utf8');
+      console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
 
-            res.on('data', function(chunk) {
+      res.on('data', function(chunk) {
 
-              console.log("Here's a chunk: ", chunk);
+        console.log("Adding a chunk... ");
+        fixtureData+= chunk;
 
-              parseString(chunk, function (err, result) {
+      });
 
-                  var inputmatches = result["XMLSOCCER.COM"].Match,
-                      outputmatches = [];
+      res.on('end',function() {
 
-                  console.log(inputmatches.length);
+        parseString(fixtureData, function (err, result) {
 
-                   for (var i = 0, len = inputmatches.length; i < len; i++) {
-                    /* console.log("Mapping match ID ", inputmatches[i].Id[0]); */
-                    if (inputmatches[i].HomeGoals !== undefined) {
-                      homegoals = parseInt(inputmatches[i].HomeGoals[0])
-                    } else {
-                      homegoals = ""
-                    }
-                    if (inputmatches[i].AwayGoals !== undefined) {
-                      awaygoals = parseInt(inputmatches[i].AwayGoals[0])
-                    } else {
-                      awaygoals = ""
-                    }
-                    match = {
-                      "_id" : inputmatches[i].Id[0],
-                      "Round" : parseInt(inputmatches[i].Round[0]),
-                      "Date" : inputmatches[i].Date[0],
-                      "HomeTeam" : inputmatches[i].HomeTeam[0],
-                      "HomeGoals" : homegoals,
-                      "AwayTeam" : inputmatches[i].AwayTeam[0],
-                      "AwayGoals" : awaygoals,
-                      "Location" : inputmatches[i].Location[0]
-                    }
-                    outputmatches.push(match);
-                  };
+          if(err) {
+              console.log('Unknown Error');
+              return;
+            }
 
-                  console.log(outputmatches);
+            var inputmatches = result["XMLSOCCER.COM"].Match,
+                outputmatches = [];
 
-                  db.collection('fixtures').insertMany(outputmatches, function(err, r) {
+            console.log(inputmatches.length);
 
-                    assert.equal(null, err);
-                    console.log('Fixtures inserted with result ', r);
+             for (var i = 0, len = inputmatches.length; i < len; i++) {
+              /* console.log("Mapping match ID ", inputmatches[i].Id[0]); */
+              if (inputmatches[i].HomeGoals !== undefined) {
+                homegoals = parseInt(inputmatches[i].HomeGoals[0])
+              } else {
+                homegoals = ""
+              }
+              if (inputmatches[i].AwayGoals !== undefined) {
+                awaygoals = parseInt(inputmatches[i].AwayGoals[0])
+              } else {
+                awaygoals = ""
+              }
+              match = {
+                "_id" : inputmatches[i].Id[0],
+                "Round" : parseInt(inputmatches[i].Round[0]),
+                "Date" : inputmatches[i].Date[0],
+                "HomeTeam" : inputmatches[i].HomeTeam[0],
+                "HomeGoals" : homegoals,
+                "AwayTeam" : inputmatches[i].AwayTeam[0],
+                "AwayGoals" : awaygoals,
+                "Location" : inputmatches[i].Location[0]
+              }
+              outputmatches.push(match);
+            };
 
-                  });
+            /* console.log(outputmatches); */
 
+            db.collection('fixtures').insertMany(outputmatches, function(err, r) {
 
-              });
+              assert.equal(null, err);
+              console.log('Fixtures inserted with result ', r);
 
             });
+
 
         });
 
       });
 
-    };
+  });
+
+});
 
     $scope.changeRound = function() {
       console.log("In changeRound", $scope.round);
